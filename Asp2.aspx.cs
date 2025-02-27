873bb3123ce772e591b760f01348975816b60dc2
kach2025 <acampos1905@gmail.com>
2025-02-26 21:08:21 -0600

    segundo commit

--- Asp2.aspx.cs
+++ Asp2.aspx.cs
@@ -11,7 +11,23 @@ namespace EjercicioAsp4_3035699
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            ddlCategory.SelectedValue = Session["ddlCategory"].ToString();
+            ddlSupplier.SelectedValue = Session["ddlSupplier"].ToString();
+            strProduct.Text = Session["strProduct"].ToString();
+            strDescription.Text = Session["strDescription"].ToString();
+            strImage.Text = Session["strImage"].ToString();
+            decimal DecPrice = Convert.ToDecimal(Session["decPrice"]);
+            decPrice.Text = DecPrice.ToString("c");
+            lblNumberInStrock.Text = Session["bytNumberInStock"].ToString();
+            lblNumberOnOrder.Text = Session["bytNumberOnOrder"].ToString();
+            bytReorderLevel.Text = Session["bytReorderLevel"].ToString();
+
+            Byte byNumberInStock = Convert.ToByte(Session["bytNumberInStock"]);
+            Byte byNumberOnOrder = Convert.ToByte(Session["bytNumberOnOrder"]);
+            decimal decValueInStock = DecPrice * byNumberInStock;
+            decimal decValueOnOrder = DecPrice * byNumberOnOrder;
+            bytNumberInStock.Text = " (Value: " + decValueInStock.ToString("c") + ")";
+            bytNumberOnOrder.Text = " (Value: " + decValueOnOrder.ToString("c") + ")";
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
